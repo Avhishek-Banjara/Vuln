44695cb8cd01e0dc694a9a762f93280cef81cdcd
Avhishek-Banjara <avhishekbanjara753@gmail.com>
2026-01-19 21:15:42 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "requests"
+        "requests",
         "colorama"# add other libraries if needed
     ],
     entry_points={
