d581581c94aa83f10fd83db81c263603dc95a0ef
Avhishek-Banjara <avhishekbanjara753@gmail.com>
2026-01-19 20:34:00 +0545

    Rename tool from 'mytool' to 'vuln' with metadata

--- tools.py
+++ tools.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mytool",                # name of your tool
+    name="vuln",                # name of your tool
     version="0.1.0",              # version number
     packages=find_packages(),     # include all code
     install_requires=[],          # add dependencies here
     entry_points={
         "console_scripts": [
-            "mytool=mytool.main:main",  # run with 'mytool' command
+            "vuln=vuln.vuln:main",  # run with 'mytool' command
         ],
     },
+    author="Avhishek",
+    description="A simple vulnerability scanner too",
+    lincense="MIT"
 )
+
